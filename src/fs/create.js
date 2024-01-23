d0d02b9f4ee2d794a7d6f9808c7439063665be86
ashtotakoe <EgorMartynov12@gmail.com>
2024-01-23 19:37:08 +0400

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
-const create = async () => {
-    // Write your code here 
-};
+import { writeFile } from 'fs/promises'
+import { existsSync } from 'fs'
+import path from 'path'
 
-await create();
+const { dirname } = import.meta
+
+const targetFilePath = path.resolve(dirname, 'files', 'hello.txt')
+
+
+const create = async () => {
+  if (!existsSync(targetFilePath)) {
+    await writeFile(targetFilePath, 'I am fresh and young')
+
+    return
+  }
+
+  throw new Error('FS operation failed')
+}
+
+await create()
+
