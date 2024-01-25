87d16353449c35a79731191be043b921a145d881
ashtotakoe <EgorMartynov12@gmail.com>
2024-01-25 22:21:48 +0400

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { mkdir, readdir, copyFile } from 'fs/promises'
+import { join } from 'path'
 
-await copy();
+const dirToCopyPath = join(import.meta.dirname, 'files')
+const dirToCopyIntoPath = join(import.meta.dirname, 'files_copy')
+
+const copy = async () => {
+  try {
+    const files = await readdir(dirToCopyPath, {
+      recursive: true,
+      withFileTypes: true,
+    })
+
+    await mkdir(dirToCopyIntoPath)
+
+    files.forEach((dirent) => {
+      if (dirent.isFile()) {
+        copyFile(join(dirToCopyPath, dirent.name), join(dirToCopyIntoPath, dirent.name))
+      }
+    })
+  } catch (err) {
+    console.log(err)
+    throw new Error('FS operation failed')
+  }
+}
+
+await copy()
