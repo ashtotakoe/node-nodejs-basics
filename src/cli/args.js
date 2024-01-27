88ccbece350da55fd052689f0f6c00e1775ac823
ashtotakoe <EgorMartynov12@gmail.com>
2024-01-27 17:25:47 +0400

    feat: implement cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+  let output = ''
+  let temp = ''
 
-parseArgs();
+  process.argv.slice(2).forEach((arg, index) => {
+    if (index % 2 === 0) {
+      temp = index === 0 ? `${arg} is ` : `, ${arg} is `
+      return
+    }
+
+    temp += arg
+    output += temp
+  })
+
+  console.log(output)
+}
+
+parseArgs()
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
-const parseEnv = () => {
-    // Write your code here 
-};
+const parseEnv = () =>
+  console.log(
+    Object.keys(process.env)
+      .filter((key) => key.startsWith('RSS_'))
+      .reduce((acc, key) => acc + `${key}=${process.env[key]}; `, '')
+  )
 
-parseEnv();
+parseEnv()
