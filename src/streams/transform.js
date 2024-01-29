fdda3d0084f9b3ab562fe8e75b30a587b45f6182
ashtotakoe <EgorMartynov12@gmail.com>
2024-01-30 02:50:28 +0400

    fix: minor changes

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,11 +1,9 @@
-import fs from 'fs'
 import { Transform, pipeline } from 'stream'
 
 const { stdin, stdout } = process
 
 const reverse = new Transform({
-  transform(chunk, encoding, callback) {
-
+  transform(chunk, _encoding, callback) {
     callback(null, `${chunk.toString().split('').reverse().join('')} \n`)
   },
 })
@@ -13,9 +11,7 @@ const reverse = new Transform({
 const transform = async () => {
   pipeline(stdin, reverse, stdout, (err) => {
     if (err) {
-      console.error('An error occured in pipeline.', err)
-    } else {
-      console.log('Pipeline execcution successful')
+      console.err(err)
     }
   })
 }
