1516716c90892e93936e9af504bd5b682107dbe5
ashtotakoe <EgorMartynov12@gmail.com>
2024-01-30 01:22:39 +0400

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from 'crypto'
+import fs from 'fs'
+import url from 'url'
+import path from 'path'
+import { Transform } from 'stream'
 
-await calculateHash();
+const { stdout } = process
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const targetPath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+
+const toSHA256 = new Transform({
+  transform(chunk, encoding, callback) {
+    const content = chunk.toString()
+
+    const hash = createHash('sha256').update(content).digest()
+
+    callback(null, hash)
+  },
+})
+
+const calculateHash = async () => {
+  const input = fs.createReadStream(targetPath)
+
+  input.pipe(toSHA256).pipe(stdout)
+}
+
+await calculateHash()
